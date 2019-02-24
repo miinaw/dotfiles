4711312d38748acb3120c10dcbea82e3628f6410
miinaw <yong.abema@gmail.com>
2019-02-24 23:42:00 +0900

    add:sh,extensions => add vscode extension settings

--- vscode_install.sh
+++ vscode_install.sh
@@ -8,3 +8,11 @@ n -s "$SCRIPT_DIR/.vscode/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
 rm "$VSCODE_SETTING_DIR/keybindings.json"
 ln -s "$SCRIPT_DIR/.vscode/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+
+# install extention
+cat ./extensions | while read line
+do
+ code --install-extension $line
+done
+
+code --list-extensions > extensions
